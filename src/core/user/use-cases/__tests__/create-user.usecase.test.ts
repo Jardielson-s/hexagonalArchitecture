6afdb4fb375fcd2e090d467456425cbe8ba2806b
jardielson silva <jardielsonbio@gmail.com>
2023-03-30 14:35:16 -0300

    test: added test config

--- src/core/user/use-cases/__tests__/create-user.usecase.test.ts
+++ src/core/user/use-cases/__tests__/create-user.usecase.test.ts
@@ -1,27 +1,9 @@
 import { describe, test, expect, vitest } from 'vitest'
-import { CreateUserUseCase, UserInput } from './create-user.usecase'
-import { UserRepository } from '@src/ports/infra/database/repositories/user.repository'
+import { CreateUserUseCase } from '../create-user.usecase'
+import { expectZodError, stub } from './user-mock'
+import { z } from 'zod'
 
-import z from 'zod'
 
-const stub = {
-	save: vitest.fn(),
-} as unknown as UserRepository<UserInput>
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-const expectZodError = async (fn: Function, expect: Function) => {
-	try {
-		await fn()
-	} catch (err) {
-		const error = err as z.ZodError
-		expect(
-			error.issues.map(({ message, path }: z.ZodIssue) => ({
-				message,
-				path: path[0],
-			})),
-		)
-	}
-}
 describe('CreateUserUseCase', () => {
 	test('should be defined execute method', () => {
 		expect(CreateUserUseCase.execute).toBeDefined()
